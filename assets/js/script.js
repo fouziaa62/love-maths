2462a8406f0a11b939c4fd7793a8f97583225049
fouziaa62 <fouziaahmad62@gmail.com>
2025-01-27 20:21:19 +0000

    added random number function

---
+++ assets/js/script.js
@@ -0,0 +1,51 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        });
+    }
+});
+
+function runGame() {
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
+   
+
+    
+}
+function checkAnswer() {
+    
+}   
+function calculateCorrectAnswer() {
+    
+}   
+function incrementScore() {
+    
+}
+function incrementWrongAnswer() {
+    
+}
+function displayAdditionQuestion() {
+    document.getElementById("operand1").textContent = num1;
+    document.getElementById("operand2").textContent = num2;
+    document.getElementById("operator").textContent = "+";
+    
+
+    
+}
+function displaySubtractQuestion() {
+    
+}
+function displayMultiplyQuestion() {
+    
+}
+function displayDivideQuestion() {
+    
+}
